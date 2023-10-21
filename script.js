ef1c0e1f7b4f030f60ac2080c1429d3a8a64f0ff
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-21 13:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click",()=>{
+	
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+
+	const outputDiv  = document.getElementById("output");
+
+	setTimeOut(()=>{
+		outputDiv.innerText = text;
+	},delay);
+})
